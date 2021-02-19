658d809eddc8b4c4ec440c17c22a426a2d8a1349
Filip Łajszczak <filip.lajszczak@gmail.com>
2021-02-19 12:27:30 +0000

    Update settings.py

--- mylist/settings.py
+++ mylist/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist.app.TodolistConfig',
+    'todolist.apps.TodolistConfig',
 ]
 
 MIDDLEWARE = [
